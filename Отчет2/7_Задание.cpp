c1ce4066c0b2cb52969eb434aa826ad1f952d8a5
JustDarkAmigo <69188060+JustDarkAmigo@users.noreply.github.com>
2025-06-19 20:03:34 +0800

    Create 7_Задание.cpp

---
+++ Отчет2/7_Задание.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool isValidCarNumber(const string& number) {
+    if (number.length() != 6) return false;
+
+    if (!isupper(number[0])) return false;
+
+    for (int i = 1; i <= 3; i++) {
+        if (!isdigit(number[i])) return false;
+    }
+
+    for (int i = 4; i < 6; i++) {
+        if (!isupper(number[i])) return false;
+    }
+
+    return true;
+}
+
+int main() {
+    string carNumber;
+    cout << "Введите номер машины: ";
+    cin >> carNumber;
+
+    if (isValidCarNumber(carNumber)) {
+        cout << "Yes\n";
+    } else {
+        cout << "No\n";
+    }
+
+    return 0;
+}
