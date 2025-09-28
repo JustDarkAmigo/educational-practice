0282a9d5982eebaae50cf3b84514904afea9cd89
JustDarkAmigo <69188060+JustDarkAmigo@users.noreply.github.com>
2025-09-28 20:16:19 +0800

    Update 7_Задание.cpp

--- Отчет2/7_Задание.cpp
+++ Отчет2/7_Задание.cpp
@@ -3,13 +3,10 @@
 using namespace std;
 bool isValidCarNumber(const string& number) {
     if (number.length() != 6) return false;
-
     if (!isupper(number[0])) return false;
-
     for (int i = 1; i <= 3; i++) {
         if (!isdigit(number[i])) return false;
     }
-
     for (int i = 4; i < 6; i++) {
         if (!isupper(number[i])) return false;
     }
