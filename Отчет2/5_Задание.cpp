46e839d38cc801d61bcc793aebcb26a99683e6dc
JustDarkAmigo <69188060+JustDarkAmigo@users.noreply.github.com>
2025-09-28 20:15:16 +0800

    Update 5_Задание.cpp

--- Отчет2/5_Задание.cpp
+++ Отчет2/5_Задание.cpp
@@ -1,21 +1,17 @@
 #include <iostream>
 #include <vector>
-
 using namespace std;
-
 int main() {
     int h, w;
     cout << "Введите высоту матрицы и её ширину через," << endl;
     cout << "а потом саму матрицу:";
     cin >> h >> w;
-
     vector<vector<int>> matrix(h, vector<int>(w));
     for (int i = 0; i < h; ++i) {
         for (int j = 0; j < w; ++j) {
             cin >> matrix[i][j];
         }
     }
-
     int minRow = h, maxRow = -1, minCol = w, maxCol = -1;
 
     for (int i = 0; i < h; ++i) {
@@ -28,8 +24,6 @@ int main() {
             }
         }
     }
-
     cout << minRow - 1 << " " << minCol - 1 << " " << maxRow + 1 << " " << maxCol + 1 << endl;
-
     return 0;
 }
