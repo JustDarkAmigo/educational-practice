717e9ca7bf0b24eceff572d12fed315eb108ff25
JustDarkAmigo <69188060+JustDarkAmigo@users.noreply.github.com>
2025-09-28 20:20:09 +0800

    Update 1_задание.cpp

--- Отчет1/1_задание.cpp
+++ Отчет1/1_задание.cpp
@@ -2,77 +2,68 @@
 #include <vector>
 #include <cstdlib>
 #include <algorithm>
-using namespace std;
-
-vector<vector<int>> hill(int n) {
-    vector<vector<int>> pyr(n);
+std::vector<std::vector<int>> Generate_Moon(int n) {
+    std::vector<std::vector<int>> pyr(n);
     for (int i = 0; i < n; ++i) {
         pyr[i].resize(i + 1);
         for (int j = 0; j <= i; ++j) {
-            pyr[i][j] = rand() % 100 + 1;
+            pyr[i][j] = std::rand() % 100 + 1;
         }
     }
     return pyr;
 }
-pair<int, vector<int>> minPath(const vector<vector<int>>& pyr) {
-    int n = pyr.size();
-    vector<vector<int>> dp(n, vector<int>(n, 0));
-    vector<vector<int>> par(n, vector<int>(n, -1));
-    dp[0][0] = pyr[0][0];
-    for (int i = 1; i < n; ++i) {
-        for (int j = 0; j <= i; ++j) {
-            if (j == 0) {
-                dp[i][j] = dp[i - 1][j] + pyr[i][j];
-                par[i][j] = j;
-            } else if (j == i) {
-                dp[i][j] = dp[i - 1][j - 1] + pyr[i][j];
-                par[i][j] = j - 1;
-            } else {
-                if (dp[i - 1][j - 1] < dp[i - 1][j]) {
-                    dp[i][j] = dp[i - 1][j - 1] + pyr[i][j];
-                    par[i][j] = j - 1;
-                } else {
-                    dp[i][j] = dp[i - 1][j] + pyr[i][j];
-                    par[i][j] = j;
-                }
-            }
-        }
+std::pair<int, std::vector<int>> min_path(const std::vector<std::vector<int>>& Moon) {
+    int height = Moon.size();
+    std::vector<std::vector<int>> Dp_Moon(height);
+    for (int i = 0; i < height; i++) {
+        Dp_Moon[i].resize(i + 3, 99999999);
     }
-    int minSum = dp[n - 1][0], minIdx = 0;
-    for (int j = 1; j < n; ++j) {
-        if (dp[n - 1][j] < minSum) {
-            minSum = dp[n - 1][j];
-            minIdx = j;
-        }
-    }
-    vector<int> path;
-    for (int i = n - 1; i >= 0; --i) {
-        path.push_back(pyr[i][minIdx]);
-        minIdx = par[i][minIdx];
-    }
-    reverse(path.begin(), path.end());
-    return {minSum, path};
-}
+    Dp_Moon[0][0] = Moon[0][0];
 
+    for (int i = 1; i < height; ++i) {
+        for (int j = 0; j <= i; ++j) {
+            int min_prev = 9999;
+            if (j > 0) min_prev = std::min(min_prev, Dp_Moon[i-1][j-1]);
+            if (j < i) min_prev = std::min(min_prev, Dp_Moon[i-1][j]);
+            if (j < i-1) min_prev = std::min(min_prev, Dp_Moon[i-1][j+1]);
+            Dp_Moon[i][j] = Moon[i][j] + min_prev;
+        }
+    }
+    int current = std::min_element(Dp_Moon[height-1].begin(), Dp_Moon[height-1].end()) - Dp_Moon[height-1].begin();
+    int time = Dp_Moon[height - 1][current];
+
+    std::vector<int> Path;
+    Path.push_back(Moon[height-1][current]);
+    for (int i = height-2; i >= 0; --i) {
+        if (current == 0) {
+            current = std::min_element(Dp_Moon[i].begin(), Dp_Moon[i].begin() + 2) - Dp_Moon[i].begin();
+        } else {
+            current = std::min_element(Dp_Moon[i].begin() + current - 1, Dp_Moon[i].begin() + current + 1) - Dp_Moon[i].begin();
+        }
+        Path.push_back(Moon[i][current]);
+    }
+    std::reverse(Path.begin(), Path.end());
+    return {time, Path};
+}
 int main() {
-    cout << "Здравствуйте, введите высоту пирамиды: ";
+    std::cout << "Здравствуйте, Введите высоту пирамиды: ";
     int n;
-    cin >> n;
-    rand();
-    auto pyr = hill(n);
-    cout << "Гора:" << endl;
+    std::cin >> n;
+    std::rand();
+    auto pyr = Generate_Moon(n);
+    std::cout << "Гора:" << std::endl;
     for (const auto& row : pyr) {
         for (size_t i = 0; i < row.size(); ++i) {
-            cout << row[i] << (i < row.size() - 1 ? " " : "");
+            std::cout << row[i] << (i < row.size() - 1 ? " " : "");
         }
-        cout << "\n";
+        std::cout << "\n";
     }
-    cout << "Результат:" << endl;
-    auto result = minPath(pyr);
-    cout << result.first << "\n";
+    std::cout << "Результат:" << std::endl;
+    auto result = min_path(pyr);
+    std::cout << result.first << "\n";
     for (size_t i = 0; i < result.second.size(); ++i) {
-        cout << result.second[i] << (i < result.second.size() - 1 ? " " : "");
+        std::cout << result.second[i] << (i < result.second.size() - 1 ? " " : "");
     }
-    cout << "\n";
+    std::cout << "\n";
     return 0;
 }
